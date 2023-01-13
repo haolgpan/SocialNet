8905867e111d9982929483242dbaf47f642a8e48
Hao <haolgpan@gmail.com>
2023-01-13 17:54:45 +0100

    It works

---
+++ app/src/main/java/com/example/socialnet/newPostFragment.java
@@ -0,0 +1,92 @@
+package com.example.socialnet;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class newPostFragment extends Fragment {
+    Button publishButton;
+    EditText postConentEditText;
+    NavController navController;
+
+
+    public newPostFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_post, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        publishButton = view.findViewById(R.id.publishButton);
+        postConentEditText = view.findViewById(R.id.postContentEditText);
+
+
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publicar();
+            }
+        });
+        navController = Navigation.findNavController(view);
+    }
+
+    void publicar() {
+        String postContent = postConentEditText.getText().toString();
+
+        if(TextUtils.isEmpty(postContent)){
+            postConentEditText.setError("Required");
+            return;
+        }
+
+        publishButton.setEnabled(false);
+
+        guardarEnFirestore(postContent);
+    }
+    private void guardarEnFirestore(String postContent) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String foto;
+        if(user.getPhotoUrl() != null) foto = user.getPhotoUrl().toString();
+        else foto = null;
+        Post post = new Post(user.getUid(), user.getDisplayName(), foto, postContent);
+
+
+        FirebaseFirestore.getInstance().collection("posts")
+                .add(post)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        navController.popBackStack();
+                    }
+                });
+    }
+}
